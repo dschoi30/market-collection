8baaebdf2f54da37014e189f39dec83591c86017
dschoi30 <ds0631@gmail.com>
2023-02-14 22:31:12 +0900

    #18 로그인 세션 호출 시 annotation 방식으로 변경

--- src/main/java/com/marketcollection/common/config/WebMvcConfig.java
+++ src/main/java/com/marketcollection/common/config/WebMvcConfig.java
@@ -1,12 +1,19 @@
 package com.marketcollection.common.config;
 
+import com.marketcollection.common.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
 
     @Value("${uploadPath}")
     String uploadPath;
@@ -15,5 +22,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/image/**")
                 .addResourceLocations(uploadPath);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
     }
 }
