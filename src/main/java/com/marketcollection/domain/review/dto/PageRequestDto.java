a867d640d71c07ccf03b149477904b4143936a10
dschoi30 <ds0631@gmail.com>
2023-03-16 21:43:48 +0900

    #36 리뷰 좋아요 기능 페이징 오류 수정

--- src/main/java/com/marketcollection/domain/review/dto/PageRequestDto.java
+++ src/main/java/com/marketcollection/domain/review/dto/PageRequestDto.java
@@ -1,12 +1,11 @@
 package com.marketcollection.domain.review.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import lombok.*;
 
-@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class PageRequestDto {
 
     @Builder.Default
@@ -14,22 +13,4 @@ public class PageRequestDto {
 
     @Builder.Default
     private int size = 10;
-
-/*    public Pageable getPageable(String... props) {
-        return PageRequest.of(this.page - 1, this.size, Sort.by(props).descending());
-    }
-
-    private String link;
-
-    public String getLink() {
-        if (link == null) {
-            StringBuilder builder = new StringBuilder();
-            builder.append("page=" + this.page);
-            builder.append("&size=" + this.size);
-
-            link = builder.toString();
-        }
-
-        return link;
-    }*/
 }
--- src/main/resources/static/js/review/review.js
+++ src/main/resources/static/js/review/review.js
@@ -1,13 +1,5 @@
-async function getReviewList({itemId, page, size, goLast}) {
+async function getReviewList({itemId, page, size}) {
     const result = await axios.get(`/reviews/${itemId}`, {params: {page, size}})
-    console.log(result);
-
-    if(goLast){
-        const total = result.data.total
-        const lastPage = parseInt(Math.ceil(total/size))
-
-        return getReviewList({itemId:itemId, page:lastPage, size:size})
-    }
 
     return result.data;
 }
