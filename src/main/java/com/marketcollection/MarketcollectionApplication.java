6db9b78ae012e2010f6ab6c9abd33d1c9d0ed629
dschoi30 <ds0631@gmail.com>
2023-03-19 20:03:06 +0900

    fixed untracked files

--- src/main/java/com/marketcollection/MarketcollectionApplication.java
+++ src/main/java/com/marketcollection/MarketcollectionApplication.java
@@ -2,12 +2,19 @@ package com.marketcollection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class MarketcollectionApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "classpath:aws.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(MarketcollectionApplication.class, args);
+		new SpringApplicationBuilder(MarketcollectionApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 
 }
