9d89d2d7eb6583ad0245be4be83d558b3197cf0e
dschoi30 <ds0631@gmail.com>
2023-03-24 11:19:04 +0900

    #41 무중단 배포 설정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname $ABSPATH)
-source ${ABSDIR}/profile.sh
+ABSDIR=$(dirname "$ABSPATH")
+source "${ABSDIR}"/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
 PROJECT_NAME=market-collection
@@ -13,13 +13,14 @@ echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 애플리케이션 배포"
+# shellcheck disable=SC2012
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행 권한 추가"
 
-chmod +x $JAR_NAME
+chmod +x "$JAR_NAME"
 
 echo "> $JAR_NAME 실행"
 
@@ -30,4 +31,4 @@ echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
   -Dspring.config.location=classpath:/application.yml,classpath:/application-real.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml,/home/ec2-user/app/application-aws.yml \
   -Dspring.profiles.active=real \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+  "$JAR_NAME" > $REPOSITORY/nohup.out 2>&1 &
