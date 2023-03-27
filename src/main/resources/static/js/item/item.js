672affe32584415cc0164e0612a735e1ad08d1dd
dschoi30 <ds0631@gmail.com>
2023-03-27 17:35:39 +0900

    #26 빌드 오류 수정

--- src/main/resources/static/js/item/item.js
+++ src/main/resources/static/js/item/item.js
@@ -15,7 +15,7 @@ var item = {
         const formData = new FormData();
         const data = {
             itemSaleStatus: $('#itemSaleStatus').val(),
-            category: $('#category').val(),
+            categoryId: $('#categoryId').val(),
             itemName: $('#itemName').val(),
             originalPrice: $('#originalPrice').val(),
             salePrice: $('#salePrice').val(),
@@ -64,7 +64,7 @@ var item = {
         const data = {
             id: $('#id').val(),
             itemSaleStatus: $('#itemSaleStatus').val(),
-            category: $('#category').val(),
+            categoryId: $('#categoryId').val(),
             itemName: $('#itemName').val(),
             originalPrice: $('#originalPrice').val(),
             salePrice: $('#salePrice').val(),
